bdfde08850a346a1e4419a7eb3c2601c917fdb38
adampeninger <adam.peninger@gmail.com>
2021-02-11 19:01:55 -0600

    Changed dependency setup to access pathfinder directly from github

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 fn main() -> std::io::Result<()> {
     let mut args: Vec<String> = std::env::args().collect();
     // Take executable path from args (read docs on std::env::args() for possible issues)
@@ -72,4 +70,18 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use pathfinder::*;
+
+    #[test]
+    fn make_flyzone() {
+        let _flyzone = vec![vec![
+            Location::from_degrees(30.32469, -97.60466, 0f32),
+            Location::from_degrees(30.32437, -97.60367, 0f32),
+            Location::from_degrees(30.32356, -97.60333, 0f32)
+        ]];
+    }
 }
