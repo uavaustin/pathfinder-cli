b1f9c89de22162fc8e4a4518061c3aa4c0187584
adampeninger <adam.peninger@gmail.com>
2021-03-08 14:48:12 -0600

    Picked parts of 'cli' and 'dep-test'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use pathfinder::*;
+
+    #[test]
+    fn make_flyzone() {
+        let _flyzone = vec![vec![
+            Location::from_degrees(30.32469, -97.60466, 0f32),
+            Location::from_degrees(30.32437, -97.60367, 0f32),
+            Location::from_degrees(30.32356, -97.60333, 0f32)
+        ]];
+    }
+}
